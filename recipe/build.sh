8a0421ff51167a89cdc106f1c4f2aabe5ef91ede
Pavel Zwerschke <pavelzw@gmail.com>
2023-05-20 21:23:40 +0200

    move to end

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,8 @@ export npm_config_build_from_source=true
 rm $PREFIX/bin/node
 ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 
-pnpm-licenses generate-disclaimer --prod > ThirdPartyLicenses.txt
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 npm install -g ${PKG_NAME}@${PKG_VERSION}
+
+pnpm-licenses generate-disclaimer --prod > ThirdPartyLicenses.txt
