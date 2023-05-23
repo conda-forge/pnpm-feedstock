f5bdcce1399ec0b60184ead33a9aab00e1e37f2a
Jannik Wibker <jannikwibker@gmail.com>
2023-05-24 00:07:50 +0200

    fix

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,8 +13,18 @@ ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
+# install pnpm globally from the npm registry
+# all things coming after this are just concerned with generating the thirdPartyLicenses.txt file
 npm install -g ${PKG_NAME}@${PKG_VERSION}
 
-pnpm install
+# pnpm uses pnpm as its package manager, which is kind of awkward to deal with sometimes
+# we thus need to use npx pnpm@latest in a few places
 
-pnpm licenses list --prod --json | npx @quantco/pnpm-licenses generate-disclaimer --json-input --filter='["@pnpm/*"]' --output-file=ThirdPartyLicenses.txt
+# there are 2 dependencies which have patches applied by pnpm, this breaks `pnpm licenses list`, thus we remove the patches
+npx pnpm@latest install
+npx pnpm@latest patch-remove pkg@5.7.0
+npx pnpm@latest patch-remove graceful-fs@4.2.11
+npx pnpm@latest install
+
+# generate the thirdPartyLicenses file using @quantco/pnpm-licenses
+npx pnpm@latest licenses list --json | npx @quantco/pnpm-licenses generate-disclaimer --json-input --filter='["@pnpm/*"]' --output-file=ThirdPartyLicenses.txt
