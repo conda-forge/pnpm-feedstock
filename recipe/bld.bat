085717c44863be42f861814cbe5e9432b888a2cf
Uwe L. Korn <uwe.korn@quantco.com>
2023-08-08 20:43:34 +0200

    Rework Windows installation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,21 @@
 @echo on
 
-call yarn licenses generate-disclaimer --prod > ThirdPartyLicenses.txt
-if errorlevel 1 exit 1
+@rem call yarn licenses generate-disclaimer --prod > ThirdPartyLicenses.txt
+@rem if errorlevel 1 exit 1
 
-call npm config set prefix %BUILD_PREFIX%
+md %LIBRARY_PREFIX%\share\pnpm
+pushd %LIBRARY_PREFIX%\share\pnpm
+md node_modules
+cmd /c "npm install pnpm@%PKG_VERSION%"
 if errorlevel 1 exit 1
+popd
 
-call npm install --userconfig nonexistentrc --global %PKG_NAME%@%PKG_VERSION%
+pushd %LIBRARY_PREFIX%\bin
+for %%c in (pnpm) do (
+  echo @echo on >> %%c.bat
+  echo "%LIBRARY_PREFIX%\share\pnpm\node_modules\.bin\%%c.cmd" %%* >> %%c.bat
+)
+popd
+
+pnpm install
 if errorlevel 1 exit 1
-
