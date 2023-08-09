ab69ed0b6c67b924f6bd63a3ce200d4456019853
Uwe L. Korn <uwe.korn@quantco.com>
2023-08-09 22:09:20 +0200

    Debugging

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -23,7 +23,8 @@ del pnpm-lock.yaml
 if errorlevel 1 exit 1
 node %RECIPE_DIR%\deletePatchedDependencies.js
 if errorlevel 1 exit 1
-pnpm install --prod
+cmd /c "pnpm install --prod"
 if errorlevel 1 exit 1
+@echo on
 npx pnpm@latest licenses list --json | npx @quantco/pnpm-licenses generate-disclaimer --json-input --filter='["@pnpm/*"]' --output-file=ThirdPartyLicenses.txt
 if errorlevel 1 exit 1
